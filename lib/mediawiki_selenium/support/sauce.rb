ee97cc08993f2a64c4ca18d87719b803c83ff9ef
Tristan Hill <tristan@saticed.me.uk>
2014-11-26 12:34:22 +0000

    change compact style module definition to nested (per rubocop)
    Bug: T75898
    Change-Id: I7967a4f04f559989afbdae7f913b3681cd6ef980

--- lib/mediawiki_selenium/support/sauce.rb
+++ lib/mediawiki_selenium/support/sauce.rb
@@ -9,21 +9,23 @@
 
 require 'cucumber/formatter/junit'
 
-module Cucumber::Formatter
-  class Sauce < Junit
+module Cucumber
+  module Formatter
+    class Sauce < Junit
 
-    private
+      private
 
-    def format_exception(exception)
-      if ENV['HEADLESS'] == 'true'
-        sauce_job_page = ''
-      elsif $session_id
-        sauce_job_page = "Sauce Labs job URL: http://saucelabs.com/jobs/#{$session_id}\n"
-      else
-        sauce_job_page = 'Uh-oh. Could not find link to Sauce Labs job URL.'
+      def format_exception(exception)
+        if ENV['HEADLESS'] == 'true'
+          sauce_job_page = ''
+        elsif $session_id
+          sauce_job_page = "Sauce Labs job URL: http://saucelabs.com/jobs/#{$session_id}\n"
+        else
+          sauce_job_page = 'Uh-oh. Could not find link to Sauce Labs job URL.'
+        end
+        ([sauce_job_page] + ["#{exception.message} (#{exception.class})"] +
+         exception.backtrace).join("\n")
       end
-      ([sauce_job_page] + ["#{exception.message} (#{exception.class})"] +
-        exception.backtrace).join("\n")
     end
   end
 end
--- spec/browser_factory/firefox_spec.rb
+++ spec/browser_factory/firefox_spec.rb
@@ -1,58 +1,60 @@
 require 'spec_helper'
 
-module MediawikiSelenium::BrowserFactory
-  describe Firefox do
-    let(:factory_class) { Firefox }
-    let(:factory) { factory_class.new(:firefox) }
+module MediawikiSelenium
+  module BrowserFactory
+    describe Firefox do
+      let(:factory_class) { Firefox }
+      let(:factory) { factory_class.new(:firefox) }
 
-    describe '.default_bindings' do
-      subject { factory_class.default_bindings }
+      describe '.default_bindings' do
+        subject { factory_class.default_bindings }
 
-      it { is_expected.to include(:browser_language) }
-      it { is_expected.to include(:browser_timeout) }
-      it { is_expected.to include(:browser_user_agent) }
-    end
-
-    describe '#browser_options' do
-      subject { factory.browser_options(config) }
-
-      let(:profile) { double(Selenium::WebDriver::Firefox::Profile) }
-
-      before do
-        expect(Selenium::WebDriver::Firefox::Profile).to receive(:new).and_return(profile)
+        it { is_expected.to include(:browser_language) }
+        it { is_expected.to include(:browser_timeout) }
+        it { is_expected.to include(:browser_user_agent) }
       end
 
-      context 'given a custom browser_timeout' do
-        let(:config) { { browser_timeout: '30' } }
+      describe '#browser_options' do
+        subject { factory.browser_options(config) }
 
-        it 'sets dom.max_script_run_time to the given number' do
-          expect(profile).to receive(:[]=).with('dom.max_script_run_time', 30)
-          allow(profile).to receive(:[]=)
-          subject
+        let(:profile) { double(Selenium::WebDriver::Firefox::Profile) }
+
+        before do
+          expect(Selenium::WebDriver::Firefox::Profile).to receive(:new).and_return(profile)
         end
 
-        it 'sets dom.max_chrome_script_run_time to the given number' do
-          expect(profile).to receive(:[]=).with('dom.max_chrome_script_run_time', 30)
-          allow(profile).to receive(:[]=)
-          subject
+        context 'given a custom browser_timeout' do
+          let(:config) { { browser_timeout: '30' } }
+
+          it 'sets dom.max_script_run_time to the given number' do
+            expect(profile).to receive(:[]=).with('dom.max_script_run_time', 30)
+            allow(profile).to receive(:[]=)
+            subject
+          end
+
+          it 'sets dom.max_chrome_script_run_time to the given number' do
+            expect(profile).to receive(:[]=).with('dom.max_chrome_script_run_time', 30)
+            allow(profile).to receive(:[]=)
+            subject
+          end
         end
-      end
 
-      context 'given a custom browser_language' do
-        let(:config) { { browser_language: 'eo' } }
+        context 'given a custom browser_language' do
+          let(:config) { { browser_language: 'eo' } }
 
-        it 'sets intl.accept_languages to the given language' do
-          expect(profile).to receive(:[]=).with('intl.accept_languages', 'eo')
-          subject
+          it 'sets intl.accept_languages to the given language' do
+            expect(profile).to receive(:[]=).with('intl.accept_languages', 'eo')
+            subject
+          end
         end
-      end
 
-      context 'given a custom browser_user_agent' do
-        let(:config) { { browser_user_agent: 'FooBot' } }
+        context 'given a custom browser_user_agent' do
+          let(:config) { { browser_user_agent: 'FooBot' } }
 
-        it 'sets general.useragent.override to the given string' do
-          expect(profile).to receive(:[]=).with('general.useragent.override', 'FooBot')
-          subject
+          it 'sets general.useragent.override to the given string' do
+            expect(profile).to receive(:[]=).with('general.useragent.override', 'FooBot')
+            subject
+          end
         end
       end
     end
